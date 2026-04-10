0e42c6b994988b020d115327a5e010b9e66f5e06
Lars Sørensen <larssorensen98@hotmail.com>
2026-04-10 11:37:24 +0200

    add opa constants and commonInitContainer spec

--- pkg/utilities/constants.go
+++ pkg/utilities/constants.go
@@ -1,5 +1,7 @@
 package utilities
 
+import corev1 "k8s.io/api/core/v1"
+
 const (
 	EgressNameSuffix = "egress"
 
@@ -17,4 +19,29 @@ const (
 	MaskinportenTestIssuer        = "https://test.maskinporten.no"
 	MaskinportenTestTokenEndpoint = "https://test.maskinporten.no/token"
 	MaskinportenTestJwksUri       = "https://test.maskinporten.no/jwk"
+
+	OpaConfigMapNameSuffix = "opa"
+	OpaDefaultPort         = int32(8181)
+)
+
+var (
+	CommonInitContainer = corev1.Container{
+		// NOTE: RestartPolicy Always is only available for init containers in Kubernetes v1.33+
+		// https://kubernetes.io/docs/concepts/workloads/pods/init-containers/#detailed-behavior
+		RestartPolicy: Ptr(corev1.ContainerRestartPolicyAlways),
+		SecurityContext: &corev1.SecurityContext{
+			AllowPrivilegeEscalation: Ptr(false),
+			Capabilities: &corev1.Capabilities{
+				Drop: []corev1.Capability{"ALL"},
+				Add:  []corev1.Capability{"NET_BIND_SERVICE"},
+			},
+			Privileged:             Ptr(false),
+			ReadOnlyRootFilesystem: Ptr(true),
+			RunAsGroup:             Ptr(int64(150)),
+			RunAsNonRoot:           Ptr(true),
+			RunAsUser:              Ptr(int64(150)),
+		},
+		TerminationMessagePath:   corev1.TerminationMessagePathDefault,
+		TerminationMessagePolicy: corev1.TerminationMessageReadFile,
+	}
 )
