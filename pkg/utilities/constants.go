b4198b0dfc4626866460b4e6375ae113d6445040
Herman Wika Horn <herman.horn@bekk.no>
2026-09-22 13:46:00 +0200

    Add trailing slash to Maskinporten issuer
    Without this trailing slash, attempts to acquire Maskinporten tokens
    fail with "Invalid JWT claim aud".

--- pkg/utilities/constants.go
+++ pkg/utilities/constants.go
@@ -19,12 +19,12 @@ const (
 	MaskinportenNameSuffix = "maskinporten"
 
 	MaskinportenProdHost          = "maskinporten.no"
-	MaskinportenProdIssuer        = "https://maskinporten.no"
+	MaskinportenProdIssuer        = "https://maskinporten.no/"
 	MaskinportenProdTokenEndpoint = "https://maskinporten.no/token"
 	MaskinportenProdJwksUri       = "https://maskinporten.no/jwk"
 
 	MaskinportenTestHost          = "test.maskinporten.no"
-	MaskinportenTestIssuer        = "https://test.maskinporten.no"
+	MaskinportenTestIssuer        = "https://test.maskinporten.no/"
 	MaskinportenTestTokenEndpoint = "https://test.maskinporten.no/token"
 	MaskinportenTestJwksUri       = "https://test.maskinporten.no/jwk"
 
